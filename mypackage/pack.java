5ef0308e0ce0cbdb97f26b70a79fe6013fa864ad
Ankit Kumar Gupta <ankitkrgupta149@gmail.com>
2022-05-31 03:18:51 +0530

    packages in java

---
+++ mypackage/pack.java
@@ -0,0 +1,7 @@
+package mypackage;
+
+public class pack {
+    public static void message() {
+        System.out.println("This comes From a Package file");
+    }
+}
---
+++ program5.java
@@ -0,0 +1,7 @@
+import mypackage.*;
+public class program5 {
+    public static void main(String[] args) {
+        pack ob = new pack();
+        pack.message();
+    }
+}
