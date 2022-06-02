754427c8893c2ebfc1589a8b798e05d5a7352408
Ankit Kumar Gupta <ankitkrgupta149@gmail.com>
2022-06-03 04:47:52 +0530

    teston finalize

--- program3.java
+++ program3.java
@@ -5,12 +5,19 @@ public class program3 {
         count++;
         System.out.println("object created with id : "+i);
     }
+    void idestroy(program3 ob){
+        System.out.println("object id "+ob.id);
+        
+    }
+    protected void finalize(){
+        System.out.println("object destroyed with id ");
+    }
     public static void main(String[] args) {
         program3 ob1 = new program3(90011);
         program3 ob2 = new program3(90012);
         program3 ob3 = new program3(90013);
         program3 ob4 = new program3(90014);
-
+        ob1.idestroy(ob2);
         System.out.println("Total objects Created "+count);
     }
 }
